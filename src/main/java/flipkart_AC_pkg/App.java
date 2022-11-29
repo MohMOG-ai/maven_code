510c2694f2c2d270b852236f6e9f56b36d097721
LakshmiIshaan <varalaxmi017@gmail.com>
2022-11-28 21:46:12 -0800

    adding flipkart code to gh

---
+++ src/main/java/flipkart_AC_pkg/App.java
@@ -0,0 +1,13 @@
+package flipkart_AC_pkg;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
